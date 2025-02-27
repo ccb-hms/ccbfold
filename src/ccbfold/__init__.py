294391cf18bb5549decf00e33a9b4c34e79659e5
Alex Pickering <alexvpickering@gmail.com>
2025-02-27 16:40:24 -0500

    init ccbfold app

---
+++ src/ccbfold/__init__.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+import click  # <- click dependency
+
+def wc(filepath: Path, show_bytes: bool, show_words: bool, show_lines: bool) -> None:
+    content = filepath.read_bytes()
+    byte_count = len(content)
+    word_count = len(content.split())
+    line_count = len(content.splitlines())
+    output = (
+        (f"{line_count:8}" if show_lines else "")
+        + (f"{word_count:8}" if show_words else "")
+        + (f"{byte_count:8}" if show_bytes else "")
+        + f" {filepath}"
+    )
+    print(output)
+
+if __name__ == "__main__":
+    wc(Path(__file__), True, True, True)
+
+@click.command()
+@click.argument("filepath", type=Path)
+@click.option("-c", is_flag=True, help="Show byte count.")
+@click.option("-w", is_flag=True, help="Show word count.")
+@click.option("-l", is_flag=True, help="Show line count.")
+def main(filepath: Path, c: bool | None, w: bool | None, l: bool | None) -> None:
+    # If none of the options was explicitly set, they're all `True`.
+    if {c, w, l} == {False}:
+        c, w, l = True, True, True
+    wc(filepath, c, w, l)
